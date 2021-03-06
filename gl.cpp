9b1f52fef4574dd9089a27fe6c4749ba2946f809
trigun <00700590@tutanota.com>
2021-03-06 11:40:47 +0300

    less 2, task 1-2 DONE!

--- gl.cpp
+++ gl.cpp
@@ -119,8 +119,14 @@ int main() {
 		glUseProgram(shaderProgramLeft);
 		float timeValue		= glfwGetTime();
 		float greenValue 	= sin(timeValue) / 2.0f + 0.5f;
+		std::cout << greenValue << std::endl;
 		int vertexColorLocation = glGetUniformLocation(shaderProgramLeft, "ourColor");
 		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+		float offset = 0.5;
+		int zap = glGetUniformLocation(shaderProgramLeft, "uniPos");
+		glUniform1f(zap, offset);
+
 		glBindVertexArray(VAOs[0]);  
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
