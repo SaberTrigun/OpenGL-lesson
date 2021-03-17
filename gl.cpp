60665fcca063562fc0327465b56e1a8782aa4be7
trigun <00700590@tutanota.com>
2021-03-17 14:51:58 +0300

    Point of returngit add .!

--- gl.cpp
+++ gl.cpp
@@ -7,6 +7,7 @@
 #include <GL/gl.h>
 #include <cmath>
 #include "loadShaders.cpp"
+#include "stb_image.h"
 // void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 // void processInput(GLFWwindow *window);
 // Обработка всех событий ввода: запрос GLFW о нажатии/отпускании клавиш на клавиатуре
@@ -69,10 +70,17 @@ int main() {
 			1, 2, 3 };
 	*/	
 	float verticesRight[] = {
-			 0.0f,-0.5f, 0.0f,	  1.0f, 0.0f, 0.0f,
-			 1.0f,-0.5f, 0.0f,	  0.0f, 1.0f, 0.0f,
-			 0.5f, 0.5f, 0.0f,	  0.0f, 0.0f, 1.0f} ;
-	
+			 0.0f,-0.5f, 0.0f,	  1.0f, 0.0f, 0.0f,	1.0f, 1.0f,
+			 1.0f,-0.5f, 0.0f,	  0.0f, 1.0f, 0.0f,	1.0f, 0.0f,
+			 0.0f, 0.5f, 0.0f,	  0.0f, 0.0f, 1.0f,	0.0f, 0.0f,
+			 1.0f, 0.5f, 0.0f,	  1.0f, 1.0f, 0.0f,	0.0f, 1.0f};
+
+	unsigned int indices [] = {
+			 0, 1, 3,
+			 1, 2, 3};
+
+
+
 
 	unsigned int shaderProgramLeft, shaderProgramRight;
 	LoadShaders("shaders/lessTwo/vertexShaderLeft.src", "shaders/lessTwo/fragmentShaderLeft.src", &shaderProgramLeft);
@@ -82,6 +90,7 @@ int main() {
 	unsigned int VBOs[2], VAOs[2], EBO;
 	glGenVertexArrays(2, VAOs);
 	glGenBuffers(2, VBOs);
+	glGenBuffers(1, &EBO);
 	
 	
 	glBindVertexArray(VAOs[0]);
@@ -90,19 +99,50 @@ int main() {
 	glVertexAttribPointer(0, 3,  GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	/*glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	
-	*/
+	
 	glBindVertexArray(VAOs[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesRight), verticesRight, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3,  GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3,  GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3,  GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glVertexAttribPointer(1, 3,  GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2,  GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+	glEnableVertexAttribArray(2);
 	
+
+	//////////////// Загрузка текстуры /////////////////
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("textures/tileTexture.jpg", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+		std::cout << "Failed to load texture" << std::endl;
+	
+	
+	stbi_image_free(data);
+	/////////////// Конец загрузки текстуры ///////////
+	
+
+
+
 
 	//glBindBuffer(GL_ARRAY_BUFFER, 0); // Отвязываем VBO
 	//glBindVertexArray(0); // Отвязываем VAO
@@ -116,12 +156,15 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		/////////// Левый /////////////////////////////
 		glUseProgram(shaderProgramLeft);
 		float timeValue		= glfwGetTime();
-		float greenValue 	= sin(timeValue) / 2.0f + 0.5f;
-		std::cout << greenValue << std::endl;
+		float redVAlue		= sin(timeValue) / 2.0f + 0.5f;
+		float greenValue 	= -sin(timeValue) / 2.0f + 0.5f;
+		float blueValue		= cos(timeValue) / 2.0f + 0.5f;
+		std::cout << blueValue<< std::endl;
 		int vertexColorLocation = glGetUniformLocation(shaderProgramLeft, "ourColor");
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		glUniform4f(vertexColorLocation, redVAlue, greenValue, blueValue, 1.0f);
 
 		float offset = 0.5;
 		int zap = glGetUniformLocation(shaderProgramLeft, "uniPos");
@@ -131,12 +174,21 @@ int main() {
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//////////////////////////////////////////////
 
+
+		////////// Правый ////////////////////////////
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glUseProgram(shaderProgramRight);
 		glBindVertexArray(VAOs[1]);  
-
-		glDrawArrays(GL_TRIANGLES, 0, 6);
 		
+		//verticesRight[4] = sin(timeValue) / 2.0f + 0.5;
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		
+		/////////////////////////////////////////////
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
