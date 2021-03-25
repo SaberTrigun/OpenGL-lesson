9f59a5bcbcd143bd0947f628b3a5b354fec9d3a6
trigun <00700590@tutanota.com>
2021-03-25 12:42:05 +0300

    less 5, part 3, drow 9 3Dsqr

--- gl.cpp
+++ gl.cpp
@@ -206,6 +206,18 @@ int main() {
 	//glBindVertexArray(0); // Отвязываем VAO
 
 	glEnable(GL_DEPTH_TEST);	
+	glm::vec3 cubePositions[] = {
+			glm::vec3( 0.0f,  0.0f,  0.0f),
+			glm::vec3( 2.0f,  5.0f, -15.0f),
+			glm::vec3(-1.5f, -2.2f, -2.5f),
+			glm::vec3(-3.8f, -2.0f, -12.3f),
+			glm::vec3( 2.4f, -0.4f, -3.5f),
+			glm::vec3(-1.7f,  3.0f, -7.5f),
+			glm::vec3( 1.3f, -2.0f, -2.5f),
+			glm::vec3( 1.5f,  2.0f, -2.5f),
+			glm::vec3( 1.5f,  0.2f, -1.5f),
+			glm::vec3(-1.3f,  1.0f, -1.5f)
+			};
 
 	// Цикл рендеринга в окне
 	while (!glfwWindowShouldClose(window))
@@ -270,9 +282,15 @@ int main() {
 		//verticesRight[4] = sin(timeValue) / 2.0f + 0.5;
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		for (int i=0; i<10; i++){
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f *i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
 		glDrawArrays(GL_TRIANGLES, 0, 36);
-		
+		}
 		/////////////////////////////////////////////
 
 		glfwSwapBuffers(window);
