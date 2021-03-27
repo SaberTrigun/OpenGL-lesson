8f3ba65d353cab76fb242abcede1e11c509926a9
trigun <00700590@tutanota.com>
2021-03-27 11:43:33 +0300

    less 5, create rotate camera DONE!

--- gl.cpp
+++ gl.cpp
@@ -219,6 +219,13 @@ int main() {
 			glm::vec3(-1.3f,  1.0f, -1.5f)
 			};
 
+	
+	glm::mat4 projection = glm::mat4(1.0f);
+	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgramRight, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+
+
 	// Цикл рендеринга в окне
 	while (!glfwWindowShouldClose(window))
 	{
@@ -260,22 +267,22 @@ int main() {
 		glUseProgram(shaderProgramRight);
 
 		glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
+		//glm::mat4 view = glm::mat4(1.0f);
 
 		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		
-		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-		int modelLoc = glGetUniformLocation(shaderProgramRight, "model");
-		int viewLoc   = glGetUniformLocation(shaderProgramRight, "view");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgramRight, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		glm::mat4 view = glm::mat4(1.0);
+		float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgramRight, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
-		
+
+
 
 		glBindVertexArray(VAOs[1]);  
 		
@@ -286,8 +293,8 @@ int main() {
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
 			float angle = 20.0f *i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+			glUniformMatrix4fv(glGetUniformLocation(shaderProgramRight, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
