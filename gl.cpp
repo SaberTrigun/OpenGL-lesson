cf1974d127d818c83ab2d6af9fe688dc3e55d282
trigun <00700590@tutanota.com>
2021-04-17 00:55:19 +0300

    Create class, load and compile, DONE!

--- gl.cpp
+++ gl.cpp
@@ -24,65 +24,60 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
+unsigned int SRC_WIDTH	= 800;
+unsigned int SRC_HEIGHT	= 600;
 
 
-
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp= glm::vec3(0.0f, 1.0f, 0.0f);
-
-
-float lastX = 800/2.0;
-float lastY = 600/2.0;
-bool firstMouse = true;
-float yaw = -90.0f;
-float pitch = 0.0f;
-float fov = 45.0f;
-
-void mouseCallback(GLFWwindow* window, double xpos, double ypos){
-	if(firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos;
-	lastX = xpos;
-	lastY = ypos;
-
-	float sensitivity = 0.05;
-	xoffset *= sensitivity;
-	yoffset *= sensitivity;
-
-	yaw += xoffset;
-	pitch += yoffset;
-	
-	if (pitch > 89.0f)
-		pitch = 89.0f;
-	if (pitch < -89.0f)
-		pitch = -89.0f;
-
-	glm::vec3 direction;
-	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-	direction.y = sin(glm::radians(pitch));
-	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-	cameraFront = glm::normalize(direction);
-}
-
-void scrollBack(GLFWwindow* window, double xoffset, double yoffset){
-	if(fov > 1.0f && fov < 45.0f)
-		fov -= yoffset;
-	else if(fov <= 1.0f)
-		fov = 1.0f;
-	else if(fov >= 45.0f)
-		fov = 45.0f;
-}
-
-const unsigned int	SRC_WIDTH = 1000;
-const unsigned int	SRC_HEIGHT = 800;
-
-
+	class Shader{
+		private:
+			unsigned int 	typeShader, shaderProgram;
+			std::string	shaderSrc;
+			int 		success;
+			char 		infolog [512];
+			char const* 	shaderSrcPtr;
+			//std::ifstream	shaderStream;
+		public:
+			void loadCompileShader(const char* shaderFilePath, std::string typeShaderIn){
+				if (typeShaderIn == "vertex"){
+					typeShader = glCreateShader(GL_VERTEX_SHADER);
+					std::ifstream shaderStream(shaderFilePath, std::ios::in);
+					if (shaderStream.is_open()){
+						std::stringstream sstr;
+						sstr << shaderStream.rdbuf();
+						shaderSrc = sstr.str();
+						shaderStream.close();
+					}
+				shaderSrcPtr = shaderSrc.c_str();
+				glShaderSource(typeShader, 1, &shaderSrcPtr, NULL);
+				glCompileShader(typeShader);
+				glGetShaderiv(typeShader, GL_COMPILE_STATUS, &success);
+				if (!success){
+					glGetShaderInfoLog(typeShader, 512, NULL, infolog);
+					std::cout << "Error vertexShader compile..." << infolog << std::endl;
+				}
+				}
+				else if (typeShaderIn == "fragment"){
+					typeShader = glCreateShader(GL_FRAGMENT_SHADER);
+					std::ifstream shaderStream(shaderFilePath, std::ios::in);
+					if (shaderStream.is_open()){
+						std::stringstream sstr;
+						sstr << shaderStream.rdbuf();
+						shaderSrc = sstr.str();
+						shaderStream.close();
+					}
+				shaderSrcPtr = shaderSrc.c_str();
+				glShaderSource(typeShader, 1, &shaderSrcPtr, NULL);
+				glCompileShader(typeShader);
+				glGetShaderiv(typeShader, GL_COMPILE_STATUS, &success);
+				if (!success){
+					glGetShaderInfoLog(typeShader, 512, NULL, infolog);
+					std::cout << "Error fragmentShader compile..." << infolog << std::endl;
+				}
+				}
+			}
+			//ShaderProgram(){}
+			//deleteShader(){}
+	};
 
 
 
@@ -104,8 +99,6 @@ int main() {
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetCursorPosCallback(window, mouseCallback);
-	glfwSetScrollCallback(window, scrollBack);
 
 	// glad загрузка всех указателей на OpenGL-функции
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -114,169 +107,9 @@ int main() {
 		return -1;
 	}
 
-
-	
-	// Указывание вершин (и буферов) и настройка вершинных атрибутов
-	float verticesLeft[] = {
-			-1.0f,-0.5f, 0.0f,	
-			 0.0f,-0.5f, 0.0f,	
-			-0.5f, 0.5f, 0.0f};
-
-	float verticesRight[] = {
-				-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-				 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-				 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-				 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-				-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-				-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-				-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-				 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-				 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-				 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-				-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-				-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-				-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-				-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-				-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-				-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-				-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-				-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-				 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-				 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-				 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-				 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-				 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-				 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-				-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-				 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-				 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-				 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-				-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-				-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-				-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-				 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-				 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-				 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-				-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-				-0.5f,  0.5f, -0.5f,  0.0f, 1.0f };
-	
-	unsigned int indices [] = {
-			 0, 1, 3,
-			 1, 2, 3};
-
-
-
-
-	unsigned int shaderProgramLeft, shaderProgramRight;
-	LoadShaders("shaders/lessFive/vertexShaderLeft.src", "shaders/lessFive/fragmentShaderLeft.src", &shaderProgramLeft);
-	LoadShaders("shaders/lessFive/vertexShaderRight.src", "shaders/lessFive/fragmentShaderRight.src", &shaderProgramRight);
-	
-
-	unsigned int VBOs[2], VAOs[2], EBO;
-	glGenVertexArrays(2, VAOs);
-	glGenBuffers(2, VBOs);
-	//glGenBuffers(1, &EBO);
-	
-	
-	glBindVertexArray(VAOs[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesLeft), verticesLeft, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3,  GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	
-	
-	glBindVertexArray(VAOs[1]);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesRight), verticesRight, GL_STATIC_DRAW);
-
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3,  GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 2,  GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
-	glEnableVertexAttribArray(1);
-	
-
-	//////////////// Загрузка текстуры /////////////////
-	unsigned int texture, texture2;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-
-	int width, height, nrChannels;
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char *data = stbi_load("textures/tileTexture.jpg", &width, &height, &nrChannels, 0);
-	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-		std::cout << "Failed to load texture" << std::endl;
-	
-	
-	stbi_image_free(data);
-	/////////////// Конец загрузки текстуры ///////////
-	
-
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-
-	data = stbi_load("textures/smileTexture.png", &width, &height, &nrChannels, 0);
-	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-		std::cout << "Failed to load texture" << std::endl;
-	
-	
-	stbi_image_free(data);
-	/////////////// Конец загрузки текстуры ///////////
-	
-
-	glUseProgram(shaderProgramRight);
-	glUniform1i(glGetUniformLocation(shaderProgramRight, "texture1"), 0);
-	glUniform1i(glGetUniformLocation(shaderProgramRight, "texture2"), 1);
-
-	//glBindBuffer(GL_ARRAY_BUFFER, 0); // Отвязываем VBO
-	//glBindVertexArray(0); // Отвязываем VAO
-
-	glEnable(GL_DEPTH_TEST);	
-	glm::vec3 cubePositions[] = {
-			glm::vec3( 0.0f,  0.0f,  0.0f),
-			glm::vec3( 2.0f,  5.0f, -15.0f),
-			glm::vec3(-1.5f, -2.2f, -2.5f),
-			glm::vec3(-3.8f, -2.0f, -12.3f),
-			glm::vec3( 2.4f, -0.4f, -3.5f),
-			glm::vec3(-1.7f,  3.0f, -7.5f),
-			glm::vec3( 1.3f, -2.0f, -2.5f),
-			glm::vec3( 1.5f,  2.0f, -2.5f),
-			glm::vec3( 1.5f,  0.2f, -1.5f),
-			glm::vec3(-1.3f,  1.0f, -1.5f)
-			};
-
-	
-	glm::mat4 projection = glm::mat4(1.0f);
-	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-	glUniformMatrix4fv(glGetUniformLocation(shaderProgramRight, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+	Shader vertexShader, fragmentShader;
+	vertexShader.loadCompileShader("shaders/lessFive/vertexShaderRight.src", "vertex");
+	fragmentShader.loadCompileShader("shaders/lessFive/fragmentShaderRight.src", "fragment");
 
 	
 
@@ -290,60 +123,11 @@ int main() {
 
 
 
-		////////// Правый ////////////////////////////
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
-
-
-
-		glUseProgram(shaderProgramRight);
-
-		glm::mat4 model = glm::mat4(1.0f);
-		//glm::mat4 view = glm::mat4(1.0f);
-
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-
-		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-		
-/*
-		glm::mat4 view = glm::mat4(1.0);
-		float radius = 10.0f;
-		float camX = sin(glfwGetTime()) * radius;
-		float camZ = cos(glfwGetTime()) * radius;
-*/
-		glm::mat4 projection = glm::perspective(glm::radians(fov), (float)SRC_WIDTH/(float)SRC_HEIGHT, 0.1f, 100.0f);
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgramRight, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-
-		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgramRight, "view"), 1, GL_FALSE, glm::value_ptr(view));
-
-		
-		
-
-
-		glBindVertexArray(VAOs[1]);  
-		
-		for (int i=0; i<10; i++){
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f *i;
-			model = glm::rotate(model, glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-			glUniformMatrix4fv(glGetUniformLocation(shaderProgramRight, "model"), 1, GL_FALSE, glm::value_ptr(model));
-
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
-		/////////////////////////////////////////////
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-
-	glDeleteVertexArrays(2, VAOs);
-	glDeleteBuffers(2, VBOs);
-	//glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 
